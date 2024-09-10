1a3ab14a6ba2205ffbb5e5148f2b37d01a7eae22
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2024-09-09 21:33:16 -0600

    Agregacion de imagen

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ const citasConfirmacion = addKeyword([
   "confirmación",
 ])
   .addAnswer([
-    "Tu sita será confirmada en unos instantes, gracias por tu preferencia 😉",
+    "Tu cita será confirmada en unos instantes, gracias por tu preferencia 😉",
   ])
   .addAnswer([
-    "Recuerda que para proceder con la confirmación de tu cita, tienes que hacer un depósito de $100 MXN a la siguiente cuenta: ",
+    "Recuerda que para proceder con la confirmación de tu cita, tienes que hacer un depósito de $100 MXN a la siguiente cuenta:\n ",
+    { media: "https://i.imgur.com/Ka3JEXm.jpeg" },
   ])
-  .addAnswer(["*0123456789*"])
   .addAnswer([
     "Recuerda, después de hacer el depósito, mandar tu comprobante. Gracias 😊",
   ]);
